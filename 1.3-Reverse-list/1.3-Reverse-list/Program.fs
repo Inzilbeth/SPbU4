7f21d3b19471dbaff6c79cf95889669aea29bc8e
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-15 02:50:53 +0300

    Improved output

--- 1.3-Reverse-list/1.3-Reverse-list/Program.fs
+++ 1.3-Reverse-list/1.3-Reverse-list/Program.fs
@@ -6,5 +6,5 @@
             | head::tail -> reverseListSub tail (head::accumulator)
     reverseListSub list []
 
-reverseList [1..10] |> List.iter (printf "%d ")
-reverseList [0] |> List.iter (printf "\n%d ")
+printfn "Original list: %A, reversed list: %A\n" [1..10] (reverseList [1..10])
+printfn "Original list: %A, reversed list: %A" [0] (reverseList [0])
