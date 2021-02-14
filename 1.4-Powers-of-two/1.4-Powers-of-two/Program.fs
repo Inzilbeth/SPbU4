def78dd6e42e99987b3f65be211b61888a704afe
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-15 02:54:12 +0300

    Improved outputs

--- 1.4-Powers-of-two/1.4-Powers-of-two/Program.fs
+++ 1.4-Powers-of-two/1.4-Powers-of-two/Program.fs
@@ -6,6 +6,6 @@
 
     [n..(n+m)] |> List.map (fun x -> pown 2 x)
 
-printfn "%A" (generatePowersOfTwoList 0 0)
-printfn "%A" (generatePowersOfTwoList 0 2)
-printfn "%A" (generatePowersOfTwoList 5 3)
+printfn "n = 0, m = 0: %A" (generatePowersOfTwoList 0 0)
+printfn "n = 0, m = 2: %A" (generatePowersOfTwoList 0 2)
+printfn "n = 5, m = 3: %A" (generatePowersOfTwoList 5 3)
