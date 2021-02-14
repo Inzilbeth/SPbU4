866adbca65a772c2b072103331791e14490f972c
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-15 02:38:51 +0300

    Implemented list generation function

---
+++ 1.4-Powers-of-two/1.4-Powers-of-two/Program.fs
@@ -0,0 +1,11 @@
+﻿let generatePowersOfTwoList n m =
+    if n < 0 then
+        failwith $"Invalid value of n: {n}. Input value must be nonnegative."
+    elif m < 0 then
+        failwith $"Invalid value of m: {m}. Input value must be nonnegative."
+
+    [n..(n+m)] |> List.map (fun x -> pown 2 x)
+
+printfn "%A" (generatePowersOfTwoList 0 0)
+printfn "%A" (generatePowersOfTwoList 0 2)
+printfn "%A" (generatePowersOfTwoList 5 3)
