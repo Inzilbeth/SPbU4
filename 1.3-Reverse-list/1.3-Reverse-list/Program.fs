8d7e030eb1b13294d120d0a54780785b7828a007
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-15 00:20:18 +0300

    Implemented reverse list function

---
+++ 1.3-Reverse-list/1.3-Reverse-list/Program.fs
@@ -0,0 +1,10 @@
+﻿let reverseList list =
+    let rec reverseListSub list accumulator =
+        match list with
+            | [] -> accumulator
+            | [x] -> x::accumulator
+            | head::tail -> reverseListSub tail (head::accumulator)
+    reverseListSub list []
+
+reverseList [1..10] |> List.iter (printf "%d ")
+reverseList [0] |> List.iter (printf "\n%d ")
