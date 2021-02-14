82c2818e5d7c3dc1845c212f5b284c90608c658d
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-14 20:08:13 +0300

    Implemented CPS factorial

---
+++ 1.1-Factorial/1.1-Factorial/1.1-Factorial/Program.fs
@@ -0,0 +1,13 @@
+﻿let contFactorial x =
+    if x < 0I then
+        failwith $"Invalid input: {x}. Input value must be nonnegative."
+
+    let rec contTailRecursiveFactorial x f =
+        if x <= 1I then
+            f()
+        else
+            contTailRecursiveFactorial (x - 1I) (fun () -> x * f())
+
+    contTailRecursiveFactorial x (fun () -> 1I)
+
+printf "Factorial of 20: %A, factorial of 0: %A." (contFactorial 20I) (contFactorial 0I)
