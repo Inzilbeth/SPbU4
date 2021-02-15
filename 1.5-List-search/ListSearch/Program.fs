6ae888afc2a646f20889e14db81b0ed6eefada1b
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-15 03:07:50 +0300

    Implemented findIndex function

---
+++ 1.5-List-search/ListSearch/Program.fs
@@ -0,0 +1,10 @@
+﻿let findIndex list value =
+    let rec findIndexSub list i =
+       match list with
+       | head :: tail when head = value -> Some(i)
+       | head :: tail -> findIndexSub tail (i + 1)
+       | [] -> None
+    findIndexSub list 0
+
+printfn "Index of 4 in [1..20]: %A" (findIndex [1..20] 4)
+printfn "Index of 777 in [1..20]: %A" (findIndex [1..20] 777)
