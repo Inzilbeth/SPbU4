2f1526dcfa022710ce0daf2ed1dad9cc01d12a1f
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-17 12:36:55 +0300

    fixed styling

--- 1.1-Factorial/Factorial/Program.fs
+++ 1.1-Factorial/Factorial/Program.fs
@@ -1,11 +1,12 @@
 ﻿let factorial x =
-    if x < 0I
-    then invalidArg "x" $"Invalid input: {x}. Input value must be nonnegative."
+    if x < 0I then
+        invalidArg "x" $"Invalid input: {x}. Input value must be nonnegative."
 
     let rec factorialRec x acc =
-        if x <= 1I
-        then acc
-        else factorialRec (x - 1I) x * acc
+        if x <= 1I then
+            acc
+        else
+            factorialRec (x - 1I) x * acc
 
     factorialRec x 1I
 
