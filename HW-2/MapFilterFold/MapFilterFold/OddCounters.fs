75d0f2b10c4b571ad278351093f469fe8e8574f3
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-20 18:24:51 +0300

    added functions

---
+++ HW-2/MapFilterFold/MapFilterFold/OddCounters.fs
@@ -0,0 +1,11 @@
+﻿namespace MapFilterFold.OddCounters
+
+module OddCounters =
+    let mapOddCounter list =
+        List.map (fun x -> (((abs (x)) + 1) % 2)) list |> List.sum
+
+    let filterOddCounter list =
+        (List.filter (fun x -> (x % 2 = 0)) list).Length
+
+    let foldOddCounter list =
+        List.fold (fun acc x -> acc + (((abs (x)) + 1) % 2)) 0 list
