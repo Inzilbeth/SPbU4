ebb1fb6743262168fca68f529e463c6503aeab28
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-24 19:14:00 +0300

    Added solution and tests

---
+++ HW-2/PrimeNumbers/PrimeNumbers/Generator.fs
@@ -0,0 +1,18 @@
+﻿namespace PrimeNumbers.Generator
+
+module Generator =
+    let rec generatePrimes =
+        let cacheUpToSqrt (s: int seq) =
+            let array = new ResizeArray<int>([|3|])
+            let e = s.GetEnumerator()
+            let cache nn =
+                    let m = array.[array.Count-1]
+                    if m*m <= nn then e.MoveNext() |> ignore; array.Add e.Current
+                    array
+            cache
+        seq {
+            yield 2; yield 3
+            let cache = cacheUpToSqrt generatePrimes
+            let isPrime n = cache n |> Seq.forall (fun i -> (n % i <> 0))
+            yield! {5 .. 2 .. System.Int32.MaxValue} |> Seq.filter isPrime
+        }
---
+++ HW-2/PrimeNumbers/Tests/Tests.fs
@@ -0,0 +1,27 @@
+﻿namespace PrimeNumbers.Tests
+
+open FsCheck
+open FsUnit
+open NUnit.Framework
+open PrimeNumbers.Generator
+
+[<TestFixture>]
+module Tests =
+    [<Test>]
+    let ``Calculates correctly for simple cases`` () =
+        Generator.generatePrimes |> Seq.item 0 |> should equal 2
+        Generator.generatePrimes |> Seq.item 1 |> should equal 3
+        Generator.generatePrimes |> Seq.item 2 |> should equal 5
+        Generator.generatePrimes |> Seq.item 1000 |> should equal 7927
+        Generator.generatePrimes |> Seq.item 10000 |> should equal 104743
+
+    [<Test>]
+    let ``Generates prime numbers`` () =
+        let isPrime number =
+            let upper = int (sqrt(float number))
+            seq {2..upper}
+            |> Seq.exists (fun x -> number % x = 0)
+            |> not
+        let ithNumberIsPrime (i : uint) =
+            Generator.generatePrimes |> Seq.item (int(i)) |> isPrime
+        Check.QuickThrowOnFailure ithNumberIsPrime
