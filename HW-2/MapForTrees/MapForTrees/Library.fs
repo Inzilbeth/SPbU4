634a473d7533a61d2a6815cd3c0d0866422ab0bf
Inzilbeth <talgat.minkashev@yandex.com>
2021-02-24 15:56:10 +0300

    Added solution and tests

--- HW-2/MapForTrees/MapForTrees/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace MapForTrees
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ HW-2/MapForTrees/MapForTrees/TreeMap.fs
@@ -0,0 +1,12 @@
+﻿namespace MapForTrees.TreeMap
+
+type Tree<'t> =
+    | Tree of 't * Tree<'t> * Tree<'t>
+    | Tip of 't
+
+module TreeMap =
+    let rec mapTree func tree =
+        match tree with
+        | Tree(node, left, right) ->
+            Tree(func node, mapTree func left, mapTree func right)
+        | Tip(node) -> Tip(func node)
