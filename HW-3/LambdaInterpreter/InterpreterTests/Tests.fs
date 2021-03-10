8a44919dd41e5c7027e503cc0c57754d094b42c9
Inzilbeth <talgat.minkashev@yandex.com>
2021-03-10 21:43:59 +0300

    Solution added

---
+++ HW-3/LambdaInterpreter/InterpreterTests/Tests.fs
@@ -0,0 +1,32 @@
+﻿namespace LambdaInterpreter.Tests
+
+open FsUnit
+open NUnit.Framework
+open LambdaInterpreter.Interpreter
+
+[<TestFixture>]
+module Tests =
+
+    [<Test>]
+    let ``Reducing II = I`` () =
+        Interpreter.reduce
+        <| Application(Abstraction('x', Variable 'x'), Abstraction('x', Variable 'x'))
+        |> should equal <| Abstraction('x', Variable 'x')
+
+    [<Test>]
+    let ``Reducing KI = K*)``() =
+        Interpreter.reduce
+        <| (Application(Abstraction('x', Abstraction('y', Variable 'x')), Abstraction('x', Variable 'x')))
+        |> should equal <| Abstraction('y', Abstraction('x', Variable 'x'))
+
+    [<Test>]
+    let ``Simple case reducing`` () =
+        Interpreter.reduce
+        <| Application(Abstraction('x', Variable 'x'), Abstraction('x', Variable 'x'))
+        |> should equal (Abstraction('x', Variable 'x'))
+
+    [<Test>]
+    let ``Substitution test`` () =
+        Interpreter.subst
+            'x' (Variable 'z') (Application(Variable 'x', Variable 'y'))
+        |> should equal (Application(Variable 'z', Variable 'y'))
---
+++ HW-3/LambdaInterpreter/LambdaInterpreter/Interpreter.fs
@@ -0,0 +1,57 @@
+﻿namespace LambdaInterpreter.Interpreter
+
+open System
+
+type Term =
+    | Variable of char
+    | Abstraction of char * Term
+    | Application of Term * Term
+
+module Interpreter =
+
+    let rec subst v leftTerm rightTerm =
+        match rightTerm with
+        | Variable rightVar when rightVar = v -> leftTerm
+        | Variable _ -> rightTerm
+        | Abstraction (x, abstTerm) ->
+
+            let getFreeVariables term =
+                let rec loop acc term =
+                    match term with
+                    | Variable v -> Set.add v acc
+                    | Abstraction (v, abstTerm) -> Set.remove v <| loop acc abstTerm
+                    | Application (applLeftTerm, applRightTerm) ->
+                        (loop acc applLeftTerm) + (loop acc applRightTerm)
+                loop Set.empty term
+
+            let getUniqueVariable set =
+                let random = Random()
+                let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
+                let size = Array.length chars in
+                chars.[random.Next size]
+
+            match leftTerm with
+            | Variable x when x = v -> rightTerm
+            | _ when getFreeVariables leftTerm |> Set.contains x |> not
+                  || getFreeVariables abstTerm |> Set.contains v |> not ->
+                     Abstraction (x, subst v leftTerm abstTerm)
+            | _ -> let y = getFreeVariables abstTerm + getFreeVariables leftTerm
+                         |> getUniqueVariable
+                   Abstraction (
+                       y, abstTerm
+                       |> subst x (Variable y)
+                       |> subst v leftTerm)
+
+        | Application (applLeftTerm, applRightTerm) ->
+            Application (
+                subst v leftTerm applLeftTerm,
+                subst v leftTerm applRightTerm)
+
+    let rec reduce term =
+        match term with
+        | Variable _ -> term
+        | Abstraction (v, abstTerm) -> Abstraction (v, reduce abstTerm)
+        | Application (leftTerm, rightTerm) ->
+            match leftTerm with
+            | Abstraction (v, abstTerm) -> reduce <| subst v rightTerm abstTerm
+            | _ -> Application (reduce leftTerm, reduce rightTerm)
