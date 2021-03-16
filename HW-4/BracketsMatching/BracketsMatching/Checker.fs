6b4e9fbee10c55fbde9e2da786390e2fc2cb5388
Inzilbeth <talgat.minkashev@yandex.com>
2021-03-16 23:48:44 +0300

    Added solution

---
+++ HW-4/BracketsMatching/BracketsMatching/Checker.fs
@@ -0,0 +1,18 @@
+﻿namespace BracketsMatching.Checker
+
+module Checker =
+
+    let check (str:string) =
+        let chars = Seq.toList str
+
+        let rec checkLoop (charSequence:List<char>) (count:List<char>) (op:char) (cl:char) =
+            match (charSequence, count) with
+            | c :: tail,  count when c = op -> checkLoop tail (op :: count) op cl
+            | c1 :: tail, c2 :: count when c1 = cl && c2 = op -> checkLoop tail count op cl
+            | c :: _, _ when c = cl -> false
+            | _ :: tail, count -> checkLoop tail count op cl
+            | [], [] -> true
+            | [], _ -> false
+
+        (* Calcualates in 3*n but otherwise match is too big and repetitive :( *)
+        checkLoop chars [] '(' ')' && checkLoop chars [] '[' ']' && checkLoop chars [] '{' '}'
---
+++ HW-4/BracketsMatching/Tests/Tests.fs
@@ -0,0 +1,26 @@
+﻿namespace BracketsMatching.Tests
+
+open System
+open FsUnit
+open NUnit.Framework
+open BracketsMatching.Checker
+
+type Tests () =
+
+    static member CheckerTestCases =
+        [|
+            "() ((())) [] {{{}}} {{(([)}])}", true
+            "() ((())) [] {{{}}} {{(([}])}", false
+            "", true
+            "(◨) (((𓁟 ))) [] {{▟ {}}ddaa} {{((gsg[)}0157])}   ", true
+            "        ", true
+            "(}", false
+            "[} []", false
+            "{{}}}{", false
+            "aaaaAaaaAa", true
+        |]
+
+    [<TestCaseSource(nameof Tests.CheckerTestCases)>]
+    member this.``Should determine correct brackets placements`` (testCase) =
+        let string, expectedResult = testCase
+        Checker.check string |> should equal expectedResult
