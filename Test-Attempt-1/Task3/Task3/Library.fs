3454aaddcd7753679b10accc41675e6b482ad9fc
Inzilbeth <talgat.minkashev@yandex.com>
2021-04-02 18:42:46 +0300

    Added task 3

---
+++ Test-Attempt-1/Task3/Task3/Library.fs
@@ -0,0 +1,19 @@
+﻿namespace Task3
+
+open System
+
+type ConcurrentStack<'t> () =
+    let mutable data : List<'t> = []
+    let obj = new Object()
+
+    member this.Push value =
+        lock obj (fun () ->
+            data <- value::data)
+
+    member this.TryPop() =
+        lock obj (fun () ->
+            match data with
+            | head::tail ->
+                data <- tail
+                Some head
+            | [] -> None)
---
+++ Test-Attempt-1/Task3/Tests/Tests.fs
@@ -0,0 +1,26 @@
+﻿namespace Tests
+
+open Task3
+open NUnit.Framework
+open FsUnit
+open System.IO
+
+module Tests =
+    [<Test>]
+    let ``Simple push test`` () =
+        let stack = new ConcurrentStack<int>()
+        stack.TryPop() |> should equal None
+        stack.Push 10
+        stack.TryPop() |> should equal (Some 10)
+
+    [<Test>]
+    let ``Simple pop test`` () =
+        let stack = new ConcurrentStack<int>()
+        stack.Push 1
+        stack.Push 2
+
+        stack.TryPop() |> should equal (Some 2)
+        stack.TryPop() |> should equal (Some 1)
+        stack.TryPop() |> should equal None
+
+    // no concurrency tests :(
