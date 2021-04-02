5d4e3e9de2519326f75b4e9a9f039f065254f2a3
Inzilbeth <talgat.minkashev@yandex.com>
2021-04-02 18:12:40 +0300

    Added task 2

---
+++ Test-Attempt-1/Task2/Tests/Tests.fs
@@ -0,0 +1,16 @@
+﻿namespace Tests
+
+open Task2
+open FsUnit
+open NUnit.Framework
+
+module Tests =
+    [<Test>]
+    let ``Simple test 1`` () =
+        Lib.supermap (fun x -> [x + 1; x * 5]) [1; 2; 3]
+            |> should equal [2; 5; 3; 10; 4; 15]
+
+    [<Test>]
+    let ``Simple test 2`` () =
+        Lib.supermap (fun x -> [x; x]) ["a"; "b"]
+            |> should equal ["a"; "a"; "b"; "b"]
