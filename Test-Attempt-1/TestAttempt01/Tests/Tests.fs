e94b01dd58aedb527db272f38e6b4633a6061e69
Inzilbeth <talgat.minkashev@yandex.com>
2021-04-02 17:47:19 +0300

    Added task 1

---
+++ Test-Attempt-1/TestAttempt01/Tests/Tests.fs
@@ -0,0 +1,26 @@
+﻿namespace Tests
+
+open Test
+open NUnit.Framework
+open FsUnit
+
+module Tests =
+    [<Test>]
+    let ``Test on n = 2`` () =
+        Test.Square.print 2 |> should equal "**\n**"
+
+    [<Test>]
+    let ``Test on n = 3`` () =
+        Test.Square.print 3 |> should equal "***\n* *\n***"
+
+    [<Test>]
+    let ``Test on n = 4`` () =
+        Test.Square.print 4 |> should equal "****\n*  *\n*  *\n****"
+
+    [<Test>]
+    let ``Test on n = 1`` () =
+        Test.Square.print 1 |> should equal "*"
+
+    [<Test>]
+    let ``Test on n = 0`` () =
+        Test.Square.print 0 |> should equal ""
